498c1cfc6610aeb9bc5e37247ce63615c9383a61
department_g33k <brannon.dan@gmail.com>
2019-12-29 23:38:52 -0800

    asdf

--- pullconfig.sh
+++ pullconfig.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd "/home/homeassistant/.homeassistant/"
+cd "bash /var/snap/docker/common/var-lib-docker/volumes/58f3283090e6f82058c64b2e8ab991d7a6d6952191f5febb8463485190dd2296/_data/"
 git pull
